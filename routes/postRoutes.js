1d16e0ad8d8c8f55fb5a816ede445c8649e9c23e
Warissara <warissara.liw@gmail.com>
2023-05-27 16:50:42 +0700

    fetch weather API

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,7 +7,29 @@ const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+async function getWeather(latitude, longitude) {
+    var requestOptions = {
+        method: 'POST',
+        redirect: 'follow'
+      };
+      
+      try {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_TOKEN}&q=${latitude},${longitude}&aqi=yes`, requestOptions);
+        const result = await response.json();
+        return result;
+      }
+      catch (error) {
+        console.log("Error:", error);
+        return null;
+      }
+}
+
 router.post("/post", upload.single("image"), async (req, res) => {
+    // const weatherData = await getWeather("16.425640290331955", "102.83526550930343");
+    console.log(req.body);
+    const weatherData = await getWeather(req.body.latitude, req.body.longitude);
+    console.log(weatherData);
+
     if (!req.file) {
         res.json({
             message: "Cannot find the file."
@@ -15,7 +37,17 @@ router.post("/post", upload.single("image"), async (req, res) => {
     }
     else {
         const data = new Post({
-            ...req.body,
+            // ...req.body,
+            owner: req.body.owner,
+            location: weatherData.location.name + ", " + weatherData.location.country,
+            condition: weatherData.current.condition.text,
+            temp: weatherData.current.temp_c,
+            wind: weatherData.current.wind_kph,
+            precip: weatherData.current.precip_mm,
+            visibility: weatherData.current.vis_km,
+            uv: weatherData.current.uv,
+            pm25: weatherData.current.air_quality.pm2_5,
+            pm25_index: weatherData.current.air_quality["us-epa-index"],
             image: {
                 data: req.file.buffer,
                 contentType: req.file.mimetype
@@ -32,20 +64,14 @@ router.post("/post", upload.single("image"), async (req, res) => {
     }
 });
 
-router.get("/", (req, res) => {
-    res.send("Get All API");
-});
-
-router.get("/getOne/:id", (req, res) => {
-    res.send("Get by ID API");
-});
-
-router.patch("/update/:id", (req, res) => {
-    res.send("Update by ID API");
-});
-
-router.delete("/delete/:id", (req, res) => {
-    res.send("Delete by ID API");
+router.get("/", async (req, res) => {
+    try {
+        const data = await Post.find();
+        res.json(data);
+    }
+    catch(err) {
+        res.status(500).json({message: error.message});
+    }
 });
 
 module.exports = router;
